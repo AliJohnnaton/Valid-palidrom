37ab98bcf4d6574723e775c1a52e0eaaa8262f1c
AliJohnnaton <trofimov2002@mail.ru>
2024-01-24 11:38:02 +0300

    Header.h

--- Valid palidrom/Valid palidrom.cpp
+++ Valid palidrom/Valid palidrom.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 bool isPalindrome(string s)
 {
-    int l;
+    int l2;
     string a;
     for (auto i : s)
     { if (isalnum(i))
             a.push_back(tolower(i)); }
-    l = a.size();
+    l2 = a.size();
     
-    for (int i(0); i < l/2; ++i)
-    {if (a[i] != a[l-i-1]) return false; }
+    for (int i(0); i < l2/2; ++i)
+    {if (a[i] != a[l2-i-1]) return false; }
     return true;
 }
 
